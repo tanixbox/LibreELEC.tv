47a995e0fc47064ca65fc3a2abc57f84857f02b4
kszaq <kszaquitto@gmail.com>
2018-01-13 21:57:09 +0100

    gpu-aml: add support for Midgard GPU
    Additionally change source repository where we can find different driver versions,
    also for testing. In case MESON_FAMILY is "gxm", use Midgard driver, use Utgard
    in all other cases.

--- packages/linux-drivers/gpu-aml/package.mk
+++ packages/linux-drivers/gpu-aml/package.mk
@@ -20,11 +20,17 @@ PKG_NAME="gpu-aml"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="GPL"
 PKG_SITE="http://openlinux.amlogic.com:8000/download/ARM/gpu/"
-# r5p1
-PKG_VERSION="9b0fbbc"
-PKG_SHA256="7ca380052b285598f1c0100cec3411f69674dcfc0f78bc2dbe407aaf85b8ebd7"
-PKG_URL="https://github.com/openwetek/gpu-aml/archive/$PKG_VERSION.tar.gz"
-PKG_SOURCE_DIR="gpu-aml-$PKG_VERSION*"
+if [ "$MESON_FAMILY" = "gxm" ] ; then
+  PKG_VERSION="fe7a4d8"
+  PKG_SHA256="518f855a2b191e50d09c2d0b3e671b5ed4b5e4db06aa3a718e29ef30cc0d9a57"
+  PKG_URL="https://github.com/khadas/android_hardware_arm_gpu/archive/$PKG_VERSION.tar.gz"
+  PKG_SOURCE_DIR="android_hardware_arm_gpu-$PKG_VERSION*"
+else
+  PKG_VERSION="9b0fbbc"
+  PKG_SHA256="7ca380052b285598f1c0100cec3411f69674dcfc0f78bc2dbe407aaf85b8ebd7"
+  PKG_URL="https://github.com/openwetek/gpu-aml/archive/$PKG_VERSION.tar.gz"
+  PKG_SOURCE_DIR="gpu-aml-$PKG_VERSION*"
+fi
 PKG_DEPENDS_TARGET="toolchain linux"
 PKG_NEED_UNPACK="$LINUX_DEPENDS"
 PKG_SECTION="driver"
@@ -34,12 +40,27 @@ PKG_TOOLCHAIN="manual"
 PKG_IS_KERNEL_PKG="yes"
 
 make_target() {
-  LDFLAGS="" make -C $(kernel_path) M=$PKG_BUILD/mali \
-    CONFIG_MALI400=m CONFIG_MALI450=m
+  if [ "$MESON_FAMILY" = "gxm" ] ; then
+    LDFLAGS="" make -C $(kernel_path) M=$PKG_BUILD/t83x/kernel/drivers/gpu/arm/midgard \
+    EXTRA_CFLAGS="-DCONFIG_MALI_PLATFORM_DEVICETREE -DCONFIG_MALI_BACKEND=gpu" \
+      CONFIG_MALI_MIDGARD=m CONFIG_MALI_PLATFORM_DEVICETREE=y CONFIG_MALI_BACKEND=gpu modules
+  else
+    LDFLAGS="" make -C $(kernel_path) M=$PKG_BUILD/mali \
+      CONFIG_MALI400=m CONFIG_MALI450=m
+  fi
 }
 
 makeinstall_target() {
-  LDFLAGS="" make -C $(kernel_path) M=$PKG_BUILD/mali \
-    INSTALL_MOD_PATH=$INSTALL/$(get_kernel_overlay_dir) INSTALL_MOD_STRIP=1 DEPMOD=: \
-  modules_install
+  if [ "$MESON_FAMILY" = "gxm" ] ; then
+    LDFLAGS="" make -C $(kernel_path) M=$PKG_BUILD/t83x/kernel/drivers/gpu/arm/midgard \
+      INSTALL_MOD_PATH=$INSTALL/$(get_kernel_overlay_dir) INSTALL_MOD_STRIP=1 DEPMOD=: \
+    modules_install
+
+    mkdir -p $INSTALL/usr/lib/modprobe.d
+    echo mali > $INSTALL/usr/lib/modprobe.d/mali.conf
+  else
+    LDFLAGS="" make -C $(kernel_path) M=$PKG_BUILD/mali \
+      INSTALL_MOD_PATH=$INSTALL/$(get_kernel_overlay_dir) INSTALL_MOD_STRIP=1 DEPMOD=: \
+    modules_install
+  fi
 }
